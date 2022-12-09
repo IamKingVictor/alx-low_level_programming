28a366510606a22cdcf7830152e2e999b1b0fd21
IamKingVictor <kingvictor306@gmail.com>
2022-12-08 18:10:33 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+nclude <stdio.h>                                                                                                                      
+
+/**                                                                                                                                     
+ *
+ *  * main - Entry                                                                                                                         
+ *
+ *   * Return: Always 0                                                                                                                     
+ *
+ *    */                                                                                                                                     
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+ printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
+ return (0);)
+}
