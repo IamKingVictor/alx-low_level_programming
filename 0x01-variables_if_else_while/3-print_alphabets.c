41af6efe6091deb6c278f83e904cf6277a5ff7cb
IamKingVictor <kingvictor306@gmail.com>
2022-12-13 10:33:36 -0800

    Print the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabets
- * 
+ * main - Prints the alphabet.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[52] =
-		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
