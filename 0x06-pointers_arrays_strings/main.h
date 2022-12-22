c2bd2fd21a261403e1a7497c148e92505da758f3
IamKingVictor <kingvictor306@gmail.com>
2022-12-21 18:06:43 -0800

    104-print_buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ int _putchar(char c);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
